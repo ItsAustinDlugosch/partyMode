32bbb8a832ccc5b4d372a1ef7acec46b5407684e
Kripa Tailor <kripa@tailor.org>
2022-04-28 14:47:48 -0500

    added letters M E and T to Letters.swift and inserted those letters to BackgroundLayer.swift

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -1,3 +1,6 @@
+
+
+
 import Igis
 import Scenes
 
@@ -11,14 +14,19 @@ import Scenes
 
 class BackgroundLayer : Layer {
     let background = Background()
-    let letterA = Letter(character: "a", start: Point(x: 500, y: 400), scale: 30)
-
+    let letterE = Letter(character: "e", start: Point(x: 500, y: 400), scale: 30)
+    let letterA = Letter(character: "a", start: Point(x: 300, y: 550), scale: 20)
+    let letterT = Letter(character: "t", start: Point(x: 100, y: 400), scale: 30)
+    let letterM = Letter(character: "m", start: Point(x: 200, y: 500), scale: 30)
       init() {
           // Using a meaningful name can be helpful for debugging
           super.init(name:"Background")
 
           // We insert our RenderableEntities in the constructor
           insert(entity:background, at:.back)
+          insert(entity:letterE, at:.front)
           insert(entity:letterA, at:.front)
+          insert(entity:letterT, at:.front)
+          insert(entity:letterM, at:.front)
       }
   }
